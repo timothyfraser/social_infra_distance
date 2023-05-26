cbd8358fc31046d822558ecc0a788ef9407a26d7
timothyfraser <timothy.fraser.1@gmail.com>
2023-05-26 18:00:13 +0000

    ---

--- commit.R
+++ commit.R
@@ -3,7 +3,14 @@
 
 #install.packages("gert")
 library(gert)
+library(credentials)
+library(usethis)
+
+
+create_github_token(scopes = c("admin:org", "repo", "workflow", "user:email"), description = "social_infra_distance")
+set_github_pat(force_new = TRUE)
 
 gert::git_add(dir(all.files = TRUE))
 gert::git_commit_all(message = "---")
-gert::git_push()
+
+
