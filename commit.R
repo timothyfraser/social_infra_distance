93d29e5728d34e9e4578cf4bef329aba9684c138
timothyfraser <timothy.fraser.1@gmail.com>
2023-05-26 17:55:55 +0000

    ---

---
+++ commit.R
@@ -0,0 +1,9 @@
+#' @name commit.R
+#' This script commits to github.
+
+#install.packages("gert")
+library(gert)
+
+gert::git_add(dir(all.files = TRUE))
+gert::git_commit_all(message = "---")
+gert::git_push()
---
+++ packages.R
@@ -0,0 +1,1 @@
+install.packages(c("googledrive", "tidyverse", "sf", "tigris", "ggtext", "ggpubr", "shadowtext", "viridis", "Zelig", "texreg", "lmtest", "ggspatial"))
